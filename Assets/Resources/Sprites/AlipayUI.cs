eefbbc66d9fbf7d81a4e3ba5ca60c808b4446b22
wxf <772616239@qq.com>
2019-03-17 17:20:26 +0800

    ups

---
+++ Assets/Resources/Sprites/AlipayUI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PayInfo
+{
+    public string subject;  // 显示在按钮上的内容,跟支付无关系  
+    public float money;     // 商品价钱  
+    public string title;    // 商品描述  
+}
+
+public class AlipayUI : MonoBehaviour
+{
+    public List<Button> buttons = null;
+    public List<PayInfo> payInfos = null;
+    private AndroidJavaObject currentActivity = null;
+
+    void Start()
+    {
+        // Init UI  
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            var payInfo = payInfos[i];
+            buttons[i].GetComponentInChildren<Text>().text = payInfos[i].subject;
+#if UNITY_ANDROID && !UNITY_EDITOR
+            buttons[i].onClick.AddListener(() =>   
+            {  
+                Alipay(payInfo);  
+            });  
+#endif
+        }
+#if UNITY_ANDROID && !UNITY_EDITOR
+// 固定写法  
+        AndroidJavaClass javaClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");  
+        currentActivity = javaClass.GetStatic<AndroidJavaObject>("currentActivity");  
+#endif
+    }
+
+    public void Alipay(PayInfo payInfo)
+    {
+        // AlipayClient是Android里的方法名字，写死.  
+        // payInfo.money是要付的钱，只能精确分.  
+        // payInfo.title是商品描述信息，注意不能有空格.  
+        currentActivity.Call("AlipayClient", payInfo.money, payInfo.title, "");
+    }
+}
---
+++ Assets/Test/TestEventDispatcher.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework;
+using UnityEngine.UI;
+
+public class TestEventDispatcher : MonoBehaviour
+{
+    public static EventDispatcher disp;
+    public int num;
+    public Button btn;
+	// Use this for initialization
+	void Start ()
+    {
+	    if (disp == null)
+	    {
+	        disp = new EventDispatcher(typeof(TestEvent));
+	    }
+	    btn = GetComponent<Button>();
+		btn.onClick.AddListener(OnClick);
+        disp.AddListener(TestEvent.OnUserClick1,Listener1);
+        disp.AddListener(TestEvent.OnUserClick2,Listener2);
+        Debug.Log(TestEvent.OnUserClick1+"#"+TestEvent.OnUserClick2);
+    }
+
+    private void OnClick()
+    {
+        if (num == 2)
+        {
+            disp.Dispatch(TestEvent.OnUserClick2,null);
+        }
+        else if(num == 1)
+        {
+            disp.Dispatch(TestEvent.OnUserClick1,null);
+            
+        }
+    }
+
+    private bool Listener1(int id, object o)
+    {
+        Debug.Log("Listener:1,id:" + id + "@" + this.GetInstanceID());
+        //disp.Dispatch(id);
+        //throw  new UnityException("salfjaiosd");
+        return false;
+    }
+    private bool Listener2(int id, object o)
+    {
+        Debug.Log("Listener:2,id:" + id + "@" + this.GetInstanceID());
+        return true;
+    }
+
+    class TestEvent :EventDispatcher.BaseEvent
+    {
+        public static int OnUserClick1 = ++id;
+        public static int OnUserClick2 = ++id;
+    }
+}
